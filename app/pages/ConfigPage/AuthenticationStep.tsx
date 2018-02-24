37595607dcc2fbf8311121dc0537e47b4bb2ca40
Tom Bowden <tom.bowden@nested.com>
2018-02-24 22:58:53 +0000

    Prettier the rest of files

--- app/pages/ConfigPage/AuthenticationStep.tsx
+++ app/pages/ConfigPage/AuthenticationStep.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {Step, StepContent, StepLabel } from "material-ui/Stepper";
+import { Step, StepContent, StepLabel } from "material-ui/Stepper";
 import Button from "material-ui/Button";
-import TextField from 'material-ui/TextField';
+import TextField from "material-ui/TextField";
 
 interface Props {
   next(): void;
@@ -9,15 +9,24 @@ interface Props {
   platform: string;
 }
 
-const AuthenticationStep: React.SFC<Props> = ({ platform, next, prev, ...rest}) => (
-  <Step {...rest }>
+const AuthenticationStep: React.SFC<Props> = ({
+  platform,
+  next,
+  prev,
+  ...rest
+}) => (
+  <Step {...rest}>
     <StepLabel>Enter your API key</StepLabel>
     <StepContent>
       <TextField id="token" label={`API key for ${platform}`} />
-      <Button size="small" variant="raised" color="primary" onClick={next}>Next</Button>
-      <Button size="small" variant="raised" onClick={prev}>Back</Button>
+      <Button size="small" variant="raised" color="primary" onClick={next}>
+        Next
+      </Button>
+      <Button size="small" variant="raised" onClick={prev}>
+        Back
+      </Button>
     </StepContent>
   </Step>
-)
+);
 
 export default AuthenticationStep;
--- app/pages/ConfigPage/ConfigPage.tsx
+++ app/pages/ConfigPage/ConfigPage.tsx
@@ -1,16 +1,13 @@
 import Button from "material-ui/Button";
-import Stepper, {Step, StepContent, StepLabel } from "material-ui/Stepper";
+import Stepper, { Step, StepContent, StepLabel } from "material-ui/Stepper";
 import React from "react";
-import {RouteComponentProps } from "react-router";
-import {
-  compose,
-  withHandlers,
-} from "recompose";
+import { RouteComponentProps } from "react-router";
+import { compose, withHandlers } from "recompose";
 import PlatformSelectStep from "./PlatformSelectStep";
 import AuthenticationStep from "./AuthenticationStep";
-import withStepNavigation, { NavigationProps } from './withStepNavigation';
-import withPlatformHandlers, {PlatformProps } from './withPlatformHandlers';
-import withValidations, {ValidationProps} from './withValidations';
+import withStepNavigation, { NavigationProps } from "./withStepNavigation";
+import withPlatformHandlers, { PlatformProps } from "./withPlatformHandlers";
+import withValidations, { ValidationProps } from "./withValidations";
 
 interface Platform {
   name: string;
@@ -21,34 +18,43 @@ interface PlatformMap {
 }
 
 const platforms = [
-  {name: "Circle CI"},
-  {name: "Jenkins"},
-  {name: "Travis"},
-]
+  { name: "Circle CI" },
+  { name: "Jenkins" },
+  { name: "Travis" }
+];
 
 const platformMap: PlatformMap = platforms.reduce(
-  (acc: PlatformMap, p: Platform) => Object.assign({}, acc, {[p.name]: p}),
+  (acc: PlatformMap, p: Platform) => Object.assign({}, acc, { [p.name]: p }),
   {}
 );
 
 type Props = NavigationProps & PlatformProps & ValidationProps;
 
-const ConfigPage: React.SFC<Props> = ({platformSelectError, validatePlatformSelection, stage, next, prev, selectPlatform, platform}) => (
+const ConfigPage: React.SFC<Props> = ({
+  platformSelectError,
+  validatePlatformSelection,
+  stage,
+  next,
+  prev,
+  selectPlatform,
+  platform
+}) => (
   <Stepper activeStep={stage} orientation="vertical">
     <PlatformSelectStep
       options={platforms}
       value={platform}
       submit={validatePlatformSelection}
       change={selectPlatform}
-      error={platformSelectError}/>
-    <AuthenticationStep
-      next={next}
-      prev={prev}
-      platform={platform!} />
+      error={platformSelectError}
+    />
+    <AuthenticationStep next={next} prev={prev} platform={platform!} />
     <Step>
       <StepLabel>Select projects</StepLabel>
-      <StepContent><h1>Select projects</h1>
-        <Button size="small" variant="raised" onClick={prev}>Back</Button>
+      <StepContent>
+        <h1>Select projects</h1>
+        <Button size="small" variant="raised" onClick={prev}>
+          Back
+        </Button>
       </StepContent>
     </Step>
   </Stepper>
@@ -57,7 +63,7 @@ const ConfigPage: React.SFC<Props> = ({platformSelectError, validatePlatformSele
 const enhance = compose<Props, {}>(
   withStepNavigation,
   withPlatformHandlers,
-  withValidations,
+  withValidations
 );
 
 export default enhance(ConfigPage);
