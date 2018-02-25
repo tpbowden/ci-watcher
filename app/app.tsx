5503c45e3b321275364b0a139e53d535ed7c219b
Tom Bowden <tom.bowden@nested.com>
2018-02-25 20:19:54 +0000

    Build bundles before testing

--- app/app.tsx
+++ app/app.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+
+import "typeface-roboto";
+
 import { render } from "react-dom";
 import { MemoryRouter, Route } from "react-router";
 
 import ConfigPage from "./pages/ConfigPage";
-import "typeface-roboto";
 
 const App = () => (
   <MemoryRouter>
