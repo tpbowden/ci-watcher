5f253300e3c87566c6bec9d0d4748429be4b4f25
Tom Bowden <tom.b1992@gmail.com>
2018-03-01 21:06:18 +0000

    Convert webpack config to typescript (#24)
    Strict types are missing for webpack 4 so that is waiting on
    merge of DefinitelyTyped/DefinitelyTyped#23958

--- webpack.config.js
+++
@@ -1,4 +0,0 @@
-const main = require("./config/webpack.main");
-const renderer = require("./config/webpack.renderer");
-
-module.exports = [main, renderer];
---
+++ webpack.config.ts
@@ -0,0 +1,5 @@
+import webpack from "webpack";
+import main from "./config/webpack.main";
+import renderer from "./config/webpack.renderer";
+
+export default [main, renderer];
