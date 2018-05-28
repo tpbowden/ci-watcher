c450a6992ffc6385a383d5375b771273ef37a5e4
Tom Bowden <tom.bowden@nested.com>
2018-05-28 15:48:24 +0100

    Fix tslint

--- src/renderer/pages/ConfigPage/ConfigPage.tsx
+++ src/renderer/pages/ConfigPage/ConfigPage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { compose, withHandlers } from "recompose";
 
-import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
+import StepLabel from "@material-ui/core/StepLabel";
+import Stepper from "@material-ui/core/Stepper";
 
 import platforms from "renderer/platforms";
 
